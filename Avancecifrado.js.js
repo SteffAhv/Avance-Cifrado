4ae8554a589198165a3914154fa294883dd0d164
SteffAhv <tefavv22@gmail.com>
2017-10-09 18:26:15 -0500

    añadiendoejercicio

---
+++ Avancecifrado.js.js
@@ -0,0 +1,32 @@
+var finalString = function(str){
+    
+    var givenString = str,
+    separador = "",
+    stringToArray = givenString.split(separador);
+
+
+ return(stringToArray);
+  
+   
+};
+
+finalString('ASDFGHJK');
+
+//=> [ 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K' ]
+
+var ascii = function(finalString){
+  
+  
+  for(var i =0;i<finalString.length;i++){
+  
+  var obtain = charCodeAt(i)
+  return obtain 
+  }
+}
+ascii(finalString);
+    
+    
+
+
+
+
